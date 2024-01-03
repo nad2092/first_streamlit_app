290c598863528e1dec0d913753816e3ea7ab99a6
nad2092 <155549448+nad2092@users.noreply.github.com>
2024-01-03 16:43:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text("🥣 Omega3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
-streamlit.text("test")
+streamlit.text("test2")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
