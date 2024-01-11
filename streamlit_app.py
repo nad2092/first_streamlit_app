313bf78ef882dc5f785e1d611aef3436abf2d2d2
nad2092 <155549448+nad2092@users.noreply.github.com>
2024-01-11 14:50:32 +0100

    Update streamlit_app.py
    removed comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,13 +24,11 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
-#Normalizing the JSON response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
